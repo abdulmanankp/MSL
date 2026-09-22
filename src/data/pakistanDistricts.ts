c357cda27b3524132a65ebaf32b82aff46796eec
Abdul Manan <ambusiness.kp@gmail.com>
2026-09-22 13:41:56 +0500

    Remove 'Narowal' from Pakistan districts list
    Removed 'Narowal' from the list of districts in Punjab.

--- src/data/pakistanDistricts.ts
+++ src/data/pakistanDistricts.ts
@@ -4,7 +4,7 @@ export const pakistanDistricts = [
   "Faisalabad", "Gujranwala", "Gujrat", "Hafizabad", "Jhang", "Jhelum", "Kasur", "Khanewal",
   "Khushab", "Lahore", "Layyah", "Lodhran", "Mandi Bahauddin", "Mianwali", "Multan",
   "Muzaffargarh", "Nankana Sahib", "Narowal", "Okara", "Pakpattan", "Rahim Yar Khan",
-  "Rajanpur", "Rawalpindi", "Sahiwal", "Sargodha", "Sheikhupura", "Sialkot", "Toba Tek Singh",
+  "Rajanpur", "Rawalpindi", "Sahiwal", "Sialkot", "Sargodha", "Sheikhupura", "Toba Tek Singh",
   "Vehari",
   // Sindh
   "Badin", "Dadu", "Ghotki", "Hyderabad", "Jacobabad", "Jamshoro", "Karachi Central",
